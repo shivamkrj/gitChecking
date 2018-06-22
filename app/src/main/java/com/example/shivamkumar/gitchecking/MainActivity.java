c9a51b182bae253f0a6b0c22cd3c8bd45e7cb649
shivamkrj <33348791+shivamkrj@users.noreply.github.com>
2018-06-23 00:00:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shivamkumar/gitchecking/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.shivamkumar.gitchecking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //hello there
+    }
+}
